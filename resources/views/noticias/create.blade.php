e44851469bb7ccac7eebaf5478fa13b787c97c31
felipemiotto <30931248+felipemiotto@users.noreply.github.com>
2017-12-04 01:57:14 -0200

    public/css/estilo.css

--- resources/views/noticias/create.blade.php
+++ resources/views/noticias/create.blade.php
@@ -5,15 +5,15 @@
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-md-8">
+<div class="row" style="position: inherit;">
+    <div class="col-md-8" style="position: inherit;">
     <form id="frm_cadastrarNoticia" enctype="multipart/form-data">
         <fieldset>
         <h2>Nova Notícia</h2>
 
             <div class="form-group{{ $errors->has('titulo') ? ' has-error' : '' }}">
-                <label for="titulo" class="col-md-4 control-label">Título</label>
-                <div class="col-md-6">
+                <label style="position: inherit" for="titulo" class="col-md-4 control-label">Título</label>
+                <div class="col-md-6" style="position: inherit">
                     <input id="titulo" type="text" class="form-control" name="titulo" value="{{ old('titulo') }}"
                             autofocus>
                     @if ($errors->has('titulo'))
@@ -25,8 +25,8 @@
             </div>
 
             <div class="form-group{{ $errors->has('subtitulo') ? ' has-error' : '' }}">
-                <label for="subtitulo" class="col-md-4 control-label">Subtítulo</label>
-                <div class="col-md-6">
+                <label for="subtitulo" class="col-md-4 control-label" style="position: inherit">Subtítulo</label>
+                <div class="col-md-6" style="position: inherit">
                     <input id="subtitulo" type="text" class="form-control" name="subtitulo"
                            value="{{ old('subtitulo') }}"  autofocus>
                     @if ($errors->has('subtitulo'))
@@ -38,8 +38,8 @@
             </div>
 
             <div class="form-group{{ $errors->has('conteudo') ? ' has-error' : '' }}">
-                <label for="conteudo" class="col-md-4 control-label">Conteúdo</label>
-                <div class="col-md-6">
+                <label style="position: inherit" for="conteudo" class="col-md-4 control-label">Conteúdo</label>
+                <div class="col-md-6" style="position: inherit">
                     <textarea id="conteudo" class="form-control" name="conteudo" 
                               autofocus>{{ old('conteudo') }}</textarea>
                     @if ($errors->has('conteudo'))
@@ -51,8 +51,8 @@
             </div>
 
             <div class="form-group{{ $errors->has('keywords') ? ' has-error' : '' }}">
-                <label for="keywords" class="col-md-4 control-label">Keywords</label>
-                <div class="col-md-6">
+                <label style="position: inherit" for="keywords" class="col-md-4 control-label">Keywords</label>
+                <div class="col-md-6" style="position: inherit">
                     <input id="keywords" type="text" class="form-control" name="keywords" value="{{ old('keywords') }}"
                            autofocus>
                     @if ($errors->has('keywords'))
@@ -64,17 +64,17 @@
             </div>
 
             <div class="form-group{{ $errors->has('publicado') ? ' has-error' : '' }}">
-                <label for="publicado" class="col-md-4 control-label">Publicar</label>
-                <div class="col-md-6">
+                <label style="position: inherit" for="publicado" class="col-md-4 control-label">Publicar</label>
+                <div class="col-md-6" style="position: inherit">
 
-                    <div class="form-check">
-                        <label class="form-check-label">
+                    <div class="form-check" style="position: inherit;">
+                        <label class="form-check-label" style="position: inherit;">
                             <input id="publicado" class="form-check-input" type="checkbox" name="publicado"
-                                   value="on" checked autofocus>
+                                   value="on" checked autofocus style="position: inherit;">
                         </label>
                     </div>
                     @if ($errors->has('publicado'))
-                        <span class="help-block">
+                        <span class="help-block" style="position: inherit;">
                             <strong>{{ $errors->first('publicado') }}</strong>
                         </span>
                     @endif
@@ -82,8 +82,8 @@
             </div>
 
             <div class="form-group{{ $errors->has('published_at') ? ' has-error' : '' }}">
-                <label for="published_at" class="col-md-4 control-label">Data de publicação</label>
-                <div class="col-md-6">
+                <label style="position: inherit" for="published_at" class="col-md-4 control-label">Data de publicação</label>
+                <div class="col-md-6" style="position: inherit">
                     <input id="published_at" type="date" name="published_at" class="form-control"
                            value="{{ old('published_at') }}"  autofocus>
                     @if ($errors->has('published_at'))
@@ -95,9 +95,9 @@
             </div>
 
             <div class="form-group{{ $errors->has('anexo') ? ' has-error' : '' }}">
-                <label for="anexo" class="col-md-4 control-label">Anexo</label>
-                <div class="col-md-6">
-                    <input id="anexo" type="file" class="form-control-file" name="anexo"
+                <label for="anexo" class="col-md-4 control-label" style="position: inherit;">Anexo</label>
+                <div class="col-md-6"style="position: inherit;">
+                    <input style="position: inherit;" id="anexo" type="file" class="form-control-file" name="anexo"
                            value="{{ old('anexo') }}" autofocus>
                     @if ($errors->has('anexo'))
                         <span class="help-block">
@@ -108,7 +108,7 @@
             </div>
 
             <div class="form-group">
-                <div class="col-md-6">
+                <div class="col-md-6" style="position: inherit">
                     <button id="btnCadastrarNoticia" type="button" class="btn btn-primary">Cadastrar</button>
                 </div>
             </div>
@@ -116,14 +116,14 @@
         </fieldset>
     </form>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4" style="position: inherit">
         <h2>Últimas Notícias</h2>
         <ul id="ultimasNoticias">
-            <li index="5">ahahahahahah</li>
-            <li index="4">bbbbbbbbbb</li>
-            <li index="3">ccccccccccc</li>
-            <li index="2">ddddddddddddd</li>
-            <li index="1">eeeeeeeeee</li>
+            <li>ahahahahahah</li>
+            <li>bbbbbbbbbb</li>
+            <li>ccccccccccc</li>
+            <li>ddddddddddddd</li>
+            <li>eeeeeeeeee</li>
 
         </ul>
     </div>
